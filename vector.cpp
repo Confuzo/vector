8908d58cd04d28202ed77bf22184527f98fc7176
Confuzo <abraaovld@gmail.com>
2018-04-29 01:10:56 -0300

    Some implementations

--- vector.cpp
+++ vector.cpp
@@ -3,7 +3,7 @@
 #include <initializer_list>
 
 namespace sc{
-	template <typename T, size_t SIZE=1> 
+	template <typename T, size_t SIZE=0>
 	class vector{
 
 		typedef T value_type;
@@ -16,7 +16,7 @@ namespace sc{
 			pointer m_size;
 			pointer m_capacity;*/
 			size_type m_size; //!< Stores the array size.
-            value_type m_data[ SIZE ]; //the storage area
+      value_type m_data[ SIZE ]; //the storage area
 			/*
 				.reserve()
 					0) ver se o tamanho alocado comporta os novos elementos
@@ -27,6 +27,67 @@ namespace sc{
 					5) m_size *= 2;
 			*/
 		public:
+			class iterator{
+					public: //iterator traits
+							typedef T*                              pointer;
+							typedef std::ptrdiff_t                  diference_type;
+							typedef T                               value_type;
+							typedef T&                              reference;
+							typedef std::bidirectional_iterator_tag iterator_category;
+
+							//constructor (empty and single value)
+							iterator(pointer ptr=nullptr) : m_ptr(ptr){
+									/*empty*/
+							}
+							//destructor
+							~iterator() = default;
+							//copy constructor
+							iterator(const iterator& itr) : m_ptr(itr.m_ptr){
+									/*empty*/
+							}
+							/// Assign operator
+							iterator& operator=(const iterator& rhs){
+									m_ptr = rhs.m_ptr;
+							}
+
+							reference operator *(void) const{
+								 return *m_ptr;
+							}
+							// ++it
+							iterator operator++(){
+									return ++m_ptr;
+							}
+							// it++
+							iterator operator++(int){
+									auto temp(*this);
+									++m_ptr;
+									return temp;
+							}
+							// --it
+							iterator operator--(){
+									return --m_ptr;
+							}
+							// it--
+							iterator operator--(int){
+									auto temp(*this);
+									--m_ptr;
+									return temp;
+							}
+
+						 bool operator==(const iterator& rhs) const{
+							return m_ptr == rhs.m_ptr;
+						 }
+						 bool operator!=(const iterator& rhs) const{
+							return m_ptr != rhs.m_ptr;
+						 }
+
+					private:
+							pointer m_ptr;
+
+			};
+			class const_iterator{
+					private:
+			};
 			//CONSTRUTORES E DESTRUTORES
 			/*Parâmetros
 				count - o tamanho da lista.
@@ -34,7 +95,7 @@ namespace sc{
 				fist, last - o intervalo para copiar os elementos de.
 				other - outra lista a ser usada como fonte para inicializar os elementos da lista com.
 				ilist - lista inicializadora para inicializar os elementos da lista com.*/
-			
+
 			//1- Construtor Default que cria uma lista vazia.
 			vector(void): m_size(SIZE){
 				/*empty*/
@@ -46,6 +107,7 @@ namespace sc{
 			//3- Constrói a lista com o conteúdo do intervalo [first, last].
 			template< typename InputIt >
 			vector( InputIt first, InputIt last ){
+					//dificuldades em implementar
 
 			}
 			//4- Construtor de cópia. Constrói a lista com a cópia profunda do conteúdo de outra.
@@ -56,7 +118,7 @@ namespace sc{
 			//5- Constrói a lista com o conteúdo da lista inicializadora init.
 			vector(const std::initializer_list<T> il ){
 				std::copy(il.begin(), il.end(), &m_data[0]);
-                m_size = il.size();
+        m_size = il.size();
 			}
 			//6- Destrói a lista. Os destruidores dos elementos são chamados e o armazenamento usado é desalocado.
 			//Note que se os elementos forem ponteiros, os objetos apontados não serão destruídos
@@ -67,22 +129,73 @@ namespace sc{
 			os dados em outro são movidos de outro para este contêiner). outro está em um válido, mas
 			estado não especificado depois*/
 			vector& operator=( const vector& other ){
-				//std::copy()
+				m_size = other.m_size;
+				std::copy(&other.m_data[0], &other.m_data[m_size], &m_data);
 			}
 			//8- Substitui o conteúdo por aqueles identificados pela lista de inicializadores ilist.
 			vector& operator=( std::initializer_list<T> ilist ){
-
+				std::copy(ilist.begin(), ilist.end(), &m_data[0]);
+        m_size = ilist.size();
 			}
 
 			reference operator[](size_type pos){
-                return m_data[pos];
-            }
+          return m_data[pos];
+      }
+
+			reference at(size_type pos){
+					if( pos < 0 or pos >= m_size)
+							throw std::out_of_range("[array::at()] Acesso fora dos limites do vetor");
+					return m_data[pos];
+			}
+
+			size_type size(void) const{
+					return m_size;
+			}
+
+			bool empty(void) const{
+					return m_size ==0;
+			}
+
+			void clear() {
+				
+			}
+
+			bool operator==(const vector<T, SIZE>& rhs){
+					if(m_size != rhs.m_size)
+							return false;
+					return std::equal(&rhs.m_data[0], &rhs.m_data[m_size], &m_data[0]);
+			}
+
+			bool operator!=(const vector<T, SIZE>& rhs){
+					if(m_size != rhs.m_size)
+							return true;
+					return std::equal(&rhs.m_data[0], &rhs.m_data[m_size], &m_data[0]);
+			}
+
+			size_type capacity( void ) const{
+				return std::distance(&m_data[0],&m_data[m_size]);
+			}
+
+			iterator begin(void){
+					return iterator(&m_data[0]);
+			}
+			iterator end(void){
+					return iterator(&m_data[m_size]);
+			}
 		};
 }
 int main(){
-	sc::vector<char> v {'c', 's'};
-	sc::vector<char> v2 (v);
-	auto a = v2[1];
-	std::cout << a << std::endl;
+	sc::vector<char, 3> a = {'c', 's','a','b'};
+	sc::vector<char> b (a.begin(),a.end());
+	for(auto it = a.begin(); it!=a.end(); it++){
+			std::cout << *it << "\n";
+	}
+
+	std::cout << a.capacity();
+	a.clear();
+	//std::cout << a.capacity();
+	/*for(auto it = b.begin(); it!=b.end(); it++){
+			std::cout << *it << "\n";
+	}*/
 	return 0;
 }
